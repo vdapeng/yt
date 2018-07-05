aadd513d81787e73de682dab85b28edfea5196e9
dapeng <fengzq@vdaoyun.com>
2018-07-05 18:44:42 +0800

    setAsyncSupported  true 异步支持

---
+++ src/main/java/com/vdaoyun/systemapi/configuration/AppInitializer.java
@@ -0,0 +1,33 @@
+package com.vdaoyun.systemapi.configuration;
+
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	
+	@Override
+	protected void customizeRegistration(Dynamic registration) {
+		registration.setInitParameter("dispatchOptionsRequest", "true");
+	    registration.setAsyncSupported(true);
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
