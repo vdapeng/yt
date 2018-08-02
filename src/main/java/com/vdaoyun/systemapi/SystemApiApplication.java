cb210cb10819a544cf162d884dde0dc2e9ca8912
dapeng <fengzq@vdaoyun.com>
2018-08-02 15:01:18 +0800

    发多少发多少佛挡杀佛

--- src/main/java/com/vdaoyun/systemapi/SystemApiApplication.java
+++ src/main/java/com/vdaoyun/systemapi/SystemApiApplication.java
@@ -8,13 +8,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.vdaoyun.systemapi.common.utils.EnvironmentUtil;
-import com.vdaoyun.systemapi.configuration.SpringContextHolder;
 
 @EnableAsync
 @SpringBootApplication
--- src/main/java/com/vdaoyun/systemapi/configuration/MybatisConfiguration.java
+++ src/main/java/com/vdaoyun/systemapi/configuration/MybatisConfiguration.java
@@ -1,17 +1,10 @@
 package com.vdaoyun.systemapi.configuration;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @MapperScan(basePackages = {"com.vdaoyun.systemapi.web", "com.vdaoyun.systemapi.wx"})//mapper扫描
 public class MybatisConfiguration {
 
-	@Bean
-	public SpringContextHolder springContextHolder() {
-		return new SpringContextHolder();
-	}
-	
-	
 }
